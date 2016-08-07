496f236250a4adda1763cce64cd8ec5608c2435c
alrachels <drachels@drachels.com>
2016-08-07 12:00:35 -0500

    Bump to new version.

--- version.php
+++ version.php
@@ -28,9 +28,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2016080200;     // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2016080700;     // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2015051100;     // Requires Moodle v2.9.
 $plugin->cron      = 60;             // Period for cron to check this module (secs).
 $plugin->component = 'mod_mootyper';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '3.1.2 (Build: 2016080200)';
+$plugin->release   = '3.1.2 (Build: 2016080700)';
