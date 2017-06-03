23e8bf102243f4e41829e4389b95e1ce39140a7f
alrachels <drachels@drachels.com>
2017-06-03 12:20:06 -0500

    Commented out unneed line of code.

--- gadd.php
+++ gadd.php
@@ -23,7 +23,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
  */
 
-defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
+//defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
 require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
 require_once(dirname(__FILE__).'/locallib.php');
