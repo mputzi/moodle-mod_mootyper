22f0338a9c4fccd0da30acf068e03cd779328f54
alrachels <drachels@drachels.com>
2017-09-28 17:00:16 -0500

    Renamed file.

--- layouts/German(SwissV3).js
+++ layouts/German(SwissV3).js
@@ -1,99 +1,9 @@
-var combinedChar = false;
-var combinedCharWait = false;
-THE_LAYOUT = 'German(SwissV3)';
-function keyboardElement(ltr) {
-    this.chr = ltr.toLowerCase();
-    this.alt = false;
-    this.uppercase_umlaut = false;
-    this.pow = false;
-    if (isLetter(ltr)) {
-        this.shift = ltr.toUpperCase() === ltr;
-    // @codingStandardsIgnoreLine
-    } else if (ltr.match(/[¦@#¬|¢´~€\[\]{}\\]/i)) {
-        this.shift = false;
-        this.alt = true;
-    } else {
-        // @codingStandardsIgnoreLine
-        if (ltr.match(/[°+"*ç%&/()=?`è!£éà<;:_]/i)) {
-            this.shift = true;
-        } else {
-            this.shift = false;
-        }
-    }
-    // @codingStandardsIgnoreLine
-    if (ltr.match(/[âêîôû]/)) {
-        this.pow = true;
-    }
-
-    if (ltr === 'Ö' || ltr === 'Ä' || ltr === 'Ü' || ltr === 'Ë') {
-        this.uppercase_umlaut = true;
-        this.shift = true;
-        this.alt = false;
-    }
-    this.turnOn = function () {  // Highlight keytop of current letter to type.
-        if (this.uppercase_umlaut) {
-            var ukey = convertFromUpperUmlaut(this.chr.toUpperCase());
-            document.getElementById('jkeyumlaut').className = 'next4';
-            document.getElementById('jkey' + ukey).className = 'next' + thenFinger(ukey);
-        } else if (this.chr === ' ') {
-            document.getElementById(getKeyID(this.chr)).className = "nextSpace";
-        } else {
-            document.getElementById(getKeyID(this.chr)).className = "next" + thenFinger(this.chr.toLowerCase());
-        }
-        if (this.chr === '\n' || this.chr === '\r\n' || this.chr === '\n\r' || this.chr === '\r') {
-            document.getElementById('jkeyenter').classname = "next4";
-        }
-        if (this.shift) {
-            document.getElementById('jkeyshiftd').className = "next4";
-            document.getElementById('jkeyshiftl').className = "next4";
-        }
-        if (this.alt) {
-            document.getElementById('jkeyaltgr').className = "nextSpace";
-        }
-        if (this.pow) {
-            document.getElementById('jkeypow').className = "next4";
-        }
-    };
-    this.turnOff = function () {  // Return last key to it's normal keytop color.
-        if (isLetter(this.chr)) {
-        // @codingStandardsIgnoreLine
-            if (this.chr.match(/[asdfjklö]/i)) {
-                document.getElementById(getKeyID(this.chr)).className = "finger" + thenFinger(this.chr.toLowerCase());
-            } else {
-                document.getElementById(getKeyID(this.chr)).className = "normal";
-            }
-        } else {
-            document.getElementById(getKeyID(this.chr)).className = "normal";
-        }
-        if (this.chr === '\n' || this.chr === '\r\n' || this.chr === '\n\r' || this.chr === '\r') {
-            document.getElementById('jkeyenter').classname = "normal";
-        }
-        if (this.shift) {
-            document.getElementById('jkeyshiftd').className = "normal";
-            document.getElementById('jkeyshiftl').className = "normal";
-        }
-        if (this.alt) {
-            document.getElementById('jkeyaltgr').className = "normal";
-        }
-        if (this.pow) {
-            document.getElementById('jkeypow').className = "normal";
-        }
-        if (this.accent) {
-            document.getElementById('jkeyrighttick').className = "normal";
-        }
-        if (this.pow) {
-            document.getElementById('jkeyrighttick').className = "normal";
-        }
-        if (this.uppercase_umlaut) {
-            document.getElementById('jkeyrighttick').className = "normal";
-        }
-    };
-}
-
 function isCombined(chr) {
-    return (chr === 'â' || chr === 'î' || chr === 'ô' || chr === 'ê' || chr === 'Ü' || chr === 'Ä' || chr === 'Ö' || chr === 'Ë' || chr === 'Û' || chr === 'Â' || chr === 'Ô' || chr === 'Ê');
-//   return false;
+    // return (chr === 'â' || chr === 'î' || chr === 'ô' || chr === 'ê' || chr === 'Ü' || chr === 'Ä' || chr === 'Ö' || chr === 'Ë' || chr === 'Û' || chr === 'Â' || chr === 'Ô' || chr === 'Ê');
+    return false;
 }
+
+THE_LAYOUT = 'German(SwissV3)';
 
 function keyupCombined(e) {
     if (ended) {
@@ -114,7 +24,7 @@ function keyupCombined(e) {
     var currentText = $('#tb1').val();
     var lastChar = currentText.substring(currentText.length - 1);
     if (combinedChar && lastChar === currentChar) {
-        if (show_keyboard){
+        if (show_keyboard) {
             var thisE = new keyboardElement(currentChar);
             thisE.turnOff();
         }
@@ -122,9 +32,9 @@ function keyupCombined(e) {
             doKonec();
             return true;
         }
-        if (currentPos < fullText.length - 1){
+        if (currentPos < fullText.length - 1) {
             var nextChar = fullText[currentPos + 1];
-            if (show_keyboard) {
+            if (show_keyboard){
                 var nextE = new keyboardElement(nextChar);
                 nextE.turnOn();
             }
@@ -138,9 +48,7 @@ function keyupCombined(e) {
         currentChar = fullText[currentPos + 1];
         currentPos++;
         return true;
-    }
-    else
-    {
+    } else {
         combinedChar = false;
         mistake++;
         var tbval = $('#tb1').val();
@@ -155,20 +63,119 @@ function keyupFirst(event) {
     return false;
 }
 
-function convertFromUpperUmlaut(c) {
-    if (c === 'Ü' || c === 'Û') {
-        return 'u';
-    } else if (c === 'Ä' || c === 'Â') {
-        return 'a';
-    } else if (c === 'Ï' || c === 'Î') {
-        return 'a';
-    } else if (c === 'Ö' || c === 'Ô') {
-        return 'o';
-    } else if (c === 'Ë' || c === 'Ê') {
-        return 'e';
+function keyboardElement(ltr) {
+    if (ltr.match(/[ÄÖÜ]/)) {
+        this.chr = ltr.toUpperCase();
     } else {
-        return null;
+        this.chr = ltr.toLowerCase();
     }
+    this.alt = false;
+    this.accent = false;
+    this.pow = false;
+    this.umlaut = false;
+    if (isLetter(ltr)) {
+        this.shift = ltr.toUpperCase() === ltr;
+    } else {
+        // @codingStandardsIgnoreLine
+        if (ltr.match(/[°+"*ç%&/()=?`è!£éà<;:_]/i)) {
+            this.shift = true;
+        } else {
+            this.shift = false;
+        }
+    }
+    // @codingStandardsIgnoreLine
+    if (ltr.match(/[¦@#¬|¢´~€\[\]{}\\]/i)) {
+        this.alt = true;
+    }
+    if (ltr.match(/[ëï]/)) {
+        this.umlaut = true;
+    }
+    if (ltr.match(/[ÄËÏÖÜ]/)) {
+        this.shift = true;
+        this.umlaut = true;
+    }
+    if (ltr.match(/[âêîôû]/)) {
+        this.pow = true;
+    }
+    if (ltr.match(/[ÂÊÎÔÛ]/)) {
+        this.shift = true;
+        this.pow = true;
+    }
+    if (ltr === 'ó' || ltr === 'á') {
+        this.alt = true;
+        this.accent = true;
+    }
+    if (ltr === 'ñ') {
+        this.alt = true;
+        this.tilde = true;
+    }
+    this.turnOn = function () {
+        if (isLetter(this.chr)) {
+            document.getElementById(getKeyID(this.chr)).className = "next" + thenFinger(this.chr.toLowerCase());
+        } else if (this.chr === ' ') {
+            document.getElementById(getKeyID(this.chr)).className = "nextSpace";
+        } else {
+            document.getElementById(getKeyID(this.chr)).className = "next" + thenFinger(this.chr.toLowerCase());
+        }
+
+        if (this.chr === '\n' || this.chr === '\r\n' || this.chr === '\n\r' || this.chr === '\r') {
+            document.getElementById('jkeyenter').className = "next4";
+        }
+        if (this.shift) {
+            document.getElementById('jkeyshiftd').className = "next4";
+            document.getElementById('jkeyshiftl').className = "next4";
+        }
+        if (this.alt) {
+            document.getElementById('jkeyaltgr').className = "next2";
+        }
+        if (this.accent) {
+            document.getElementById('jkeyù').className = "next4";
+        }
+        if (this.pow) {
+            document.getElementById('jkeypow').className = "next4";
+        }
+        if (this.tilde) {
+            document.getElementById('jkeyequal').className = "next4";
+        }
+        if (this.umlaut) {
+            document.getElementById('jkeyumlaut').className = "next4";
+        }
+    };
+    this.turnOff = function () {
+        if (isLetter(this.chr)) {
+            // @codingStandardsIgnoreLine
+            if (this.chr.match(/[asdfjklö]/i)) {
+                document.getElementById(getKeyID(this.chr)).className = "finger" + thenFinger(this.chr.toLowerCase());
+            } else {
+                document.getElementById(getKeyID(this.chr)).className = "normal";
+            }
+        } else {
+            document.getElementById(getKeyID(this.chr)).className = "normal";
+        }
+
+        if (this.chr === '\n' || this.chr === '\r\n' || this.chr === '\n\r' || this.chr === '\r') {
+            document.getElementById('jkeyenter').classname = "normal";
+        }
+        if (this.shift) {
+            document.getElementById('jkeyshiftd').className = "normal";
+            document.getElementById('jkeyshiftl').className = "normal";
+        }
+        if (this.alt) {
+            document.getElementById('jkeyaltgr').className = "normal";
+        }
+        if (this.accent) {
+            document.getElementById('jkeyù').className = "normal";
+        }
+        if (this.pow) {
+            document.getElementById('jkeypow').className = "normal";
+        }
+        if (this.tilde) {
+            document.getElementById('jkeyequal').className = "normal";
+        }
+        if (this.umlaut) {
+            document.getElementById('jkeyumlaut').className = "normal";
+        }
+    };
 }
 
 function thenFinger(t_crka) {
@@ -178,13 +185,13 @@ function thenFinger(t_crka) {
     } else if (t_crka.match(/[\n§°1+¦qaây<>\\0=pöé.:'?´üè\[äà{\-_^`~¨!\]$£}]/i)) {
         return 4; // Highlight the correct key above in red.
     // @codingStandardsIgnoreLine
-    } else if (t_crka.match(/[2"@wsx9)ol.:]/i)) {
+    } else if (t_crka.match(/[2"@wsx9)oôöl.:]/i)) {
         return 3; // Highlight the correct key above in green.
     // @codingStandardsIgnoreLine
-    } else if (t_crka.match(/[3*#eê€dc8(¢iîk,;]/i)) {
+    } else if (t_crka.match(/[3*#eëê€dc8(¢iïîk,;]/i)) {
         return 2; // Highlight the correct key above in yellow.
     // @codingStandardsIgnoreLine
-    } else if (t_crka.match(/[4çrfv5%tgb6&¬zhn7//|ujm]/i)) {
+    } else if (t_crka.match(/[4çrfv5%tgb6&¬zhn7//|uûjm]/i)) {
         return 1; // Highlight the correct key above in blue.
     } else {
         return 6; // Do not change any highlight.
@@ -222,22 +229,24 @@ function getKeyID(t_crka) {
         return "jkeyapostrophe";
     } else if (t_crka === '^' || t_crka === '~' || t_crka === '`') {
         return "jkeypow";
-    } else if (t_crka === '€' || t_crka === 'ë' || t_crka === 'ê') {
+    } else if (t_crka.match(/[€ëËê]/)) {
         return "jkeye";
-    } else if (t_crka === 'u' || t_crka === 'û') {
+    } else if (t_crka.match(/[uûÜ]/)) {
         return "jkeyu";
     } else if (t_crka === 'i' || t_crka === 'î' || t_crka === 'í' || t_crka === 'ï') {
         return "jkeyi";
-    } else if (t_crka === 'o' || t_crka === 'ô' || t_crka === 'ó') {
+    } else if (t_crka.match(/[oôÖó]/)) {
         return "jkeyo";
     } else if (t_crka === 'ü' || t_crka === 'è' || t_crka === '[') {
         return "jkeyü";
     } else if (t_crka === '¨' || t_crka === '!' || t_crka === ']') {
-        return "jkeygerklicaj";
+        return "jkeyumlaut";
     } else if (t_crka === 'ö' || t_crka === 'é') {
         return "jkeyö";
-    } else if (t_crka === 'ä' || t_crka === 'à' || t_crka === '{') {
-        return "jkeyä";
+    } else if (t_crka.match(/[Äâ]/)) {
+        return "jkeya";
+    } else if (t_crka.match(/[äà{]/)) {
+        return "jkeyumlauta";
     } else if ( t_crka === '$' || t_crka === '£' || t_crka === '}') {
         return "jkeydollar";
     } else if (t_crka === ',' || t_crka === ';') {
@@ -248,8 +257,6 @@ function getKeyID(t_crka) {
         return "jkeyminus";
     } else if (t_crka === '=' || t_crka === '+' || t_crka === '~') {
         return "jkeyequal";
-    } else if (t_crka.match(/[Äâ]/)) {
-        return "jkeya";
     } else if (t_crka === '<' || t_crka === '>') {
         return "jkeyckck";
     } else if (t_crka === 'ñ') {
@@ -260,6 +267,5 @@ function getKeyID(t_crka) {
 }
 
 function isLetter(str) {
-//    return str.length === 1 && str.match(/[a-zâêîôûčšžđćüöä]/i);
-    return str.length === 1 && str.match(/[a-z]/i);
+    return str.length === 1 && str.match(/[a-záíóúüù]/i);
 }
--- layouts/German(SwissV3).php
+++ layouts/German(SwissV3).php
@@ -15,7 +15,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * This file defines the German(SwissV3)keyboard layout.
+ * This file defines the German(SwissV3) keyboard layout.
  *
  * @package    mod_mootyper
  * @copyright  2016 onwards AL Rachels (drachels@drachels.com)
@@ -26,38 +26,37 @@
 ?>
 <div id="innerKeyboard" style="margin: 0px auto;display: inline-block;
 <?php
-echo (isset($displaynone) && $displaynone == true) ? 'display:none;' : '';
+echo (isset($displaynone) && ($displaynone == true)) ? 'display:none;' : '';
 ?>
 "><br>
 <span id="keyboard" class="keyboardback">German(SwissV3) Keyboard Layout<br><br>
-<span id="jkeysection" class="normal"><span class="textup">° </span><span class="textdown"> §</span></span>
-<span id="jkey1" class="normal"><span class="textup">+</span><span class="textdown"> 1 </span>
-    <span class="textdown" style="color:blue">¦</span></span>
+<span id="jkeysection" class="normal"><span class="textup">° </span><span class="textdown">§</span></span>
+<span id="jkey1" class="normal"><span class="textup">+</span><span class="textdown">1</span>
+    <span class="textdown" style="color:blue"> ¦</span></span>
 <span id="jkey2" class="normal"><span class="textup">"</span><span class="textdown">2</span>
-    <span class="textdown" style="color:blue">@</span></span>
+    <span class="textdown" style="color:blue"> @</span></span>
 <span id="jkey3" class="normal"><span class="textup">*</span><span class="textdown">3</span>
-    <span class="textdown" style="color:blue">#</span></span>
-<span id="jkey4" class="normal"><span class="textup">ç</span><span class="textdown"> 4</span></span>
-<span id="jkey5" class="normal"><span class="textup">%</span><span class="textdown"> 5</span></span>
-<span id="jkey6" class="normal"><span class="textup">& </span><span class="textdown">6</span>
-    <span class="textdown" style="color:blue">¬</span></span>
+    <span class="textdown" style="color:blue"> #</span></span>
+<span id="jkey4" class="normal"><span class="textup">ç</span><span class="textdown">4</span></span>
+<span id="jkey5" class="normal"><span class="textup">%</span><span class="textdown">5</span></span>
+<span id="jkey6" class="normal"><span class="textup">&</span><span class="textdown">6</span>
+    <span class="textdown" style="color:red"> ¬</span></span>
 <span id="jkey7" class="normal"><span class="textup">/</span><span class="textdown">7</span>
     <span class="textdown" style="color:blue">|</span></span>
 <span id="jkey8" class="normal"><span class="textup">(</span><span class="textdown">8</span>
     <span class="textdown" style="color:blue">¢</span></span>
-<span id="jkey9" class="normal"><span class="textup">)</span><span class="textdown"> 9</span></span>
-<span id="jkey0" class="normal"><span class="textup">=</span><span class="textdown"> 0</span></span>
-<span id="jkeyapostrophe" class="normal"><span class="textup">?</span><span class="textdown"> '</span>
+<span id="jkey9" class="normal"><span class="textup">)</span><span class="textdown">9</span></span>
+<span id="jkey0" class="normal"><span class="textup">=</span><span class="textdown">0</span></span>
+<span id="jkeyapostrophe" class="normal"><span class="textup">? </span><span class="textdown"> '</span>
     <span class="textdown" style="color:red">´</span></span>
-<span id="jkeypow" class="normal"><span class="textup">`</span><span class="textdown"> ^</span>
-    <span class="textdown" style="color:red">~</span></span>
+<span id="jkeypow" class="normal"><span class="textup">` </span><span class="textdown" style="color:red">^ ~</span></span>
 <span id="jkeybackspace" class="normal" style="border-right-style: solid;">Backspace</span>
 <br>
 <div style="float: left;">
 <span id="jkeytab" class="normal" style="width: 54px;">Tab</span>
 <span id="jkeyq" class="normal">Q</span>
 <span id="jkeyw" class="normal">W</span>
-<span id="jkeye" class="normal">E<span class="textdown" style="color:blue"> €</span></span>
+<span id="jkeye" class="normal">E<span style="color:blue" class="textdown"> €</span></span>
 <span id="jkeyr" class="normal">R</span>
 <span id="jkeyt" class="normal">T</span>
 <span id="jkeyz" class="normal">Z</span>
@@ -70,7 +69,7 @@ echo (isset($displaynone) && $displaynone == true) ? 'display:none;' : '';
 <span id="jkeyumlaut" class="normal"><span class="textup">!</span><span class="textdown"> ¨</span>
     <span class="textdown" style="color:blue">]</span></span>
 <br>
-<span id="jkeycaps" class="normal" style="width: 60px;">C.lock</span>
+<span id="jkeycaps" class="normal" style="width: 60px; font-size: 12px !important;">Caps Lock</span>
 <span id="jkeya" class="finger4">A</span>
 <span id="jkeys" class="finger3">S</span>
 <span id="jkeyd" class="finger2">D</span>
@@ -81,17 +80,17 @@ echo (isset($displaynone) && $displaynone == true) ? 'display:none;' : '';
 <span id="jkeyk" class="finger2">K</span>
 <span id="jkeyl" class="finger3">L</span>
 <span id="jkeyö" class="finger4"><span class="textup">é</span><span class="textdown"> ö</span></span>
-<span id="jkeyä" class="normal"><span class="textup">à</span><span class="textdown">ä</span>
+<span id="jkeyumlauta" class="normal"><span class="textup">à</span><span class="textdown">ä</span>
     <span class="textdown" style="color:blue"> {</span></span>
 <span id="jkeydollar" class="normal"><span class="textup">£</span><span class="textdown">$</span>
     <span class="textdown" style="color:blue">}</span></span>
 </div>
-<span id="jkeyenter" class="normal" style="width: 50px; float: right; height: 102px;">Enter</span>
+<span id="jkeyenter" class="normal" style="width: 50px; border-right-style: solid; float: right; height: 105px;">Enter</span>
 <br style="clear:both;" /><br />
 
-<span id="jkeyshiftl" class="normal" style="width: 72px;">Shift</span>
-<span id="jkeyckck" class="normal"><span class="textup">&lt;</span><span class="textdown"> &gt; </span>
-    <span class="textdown" style="color:blue">\</span></span>
+<span id="jkeyshiftl" class="normal" style="width: 45px;">Shift</span>
+<span id="jkeyckck" class="normal"><span class="textup">&lt;</span><span class="textdown"> &gt;</span>
+    <span class="textdown" style="color:blue"> \</span></span>
 <span id="jkeyy" class="normal">Y</span>
 <span id="jkeyx" class="normal">X</span>
 <span id="jkeyc" class="normal">C</span>
@@ -99,18 +98,18 @@ echo (isset($displaynone) && $displaynone == true) ? 'display:none;' : '';
 <span id="jkeyb" class="normal">B</span>
 <span id="jkeyn" class="normal">N</span>
 <span id="jkeym" class="normal">M</span>
-<span id="jkeycomma" class="normal"><span class="textup">;</span><span class="textdown"> ,</span></span>
-<span id="jkeyperiod" class="normal"><span class="textup">:</span><span class="textdown"> .</span></span>
-<span id="jkeyminus" class="normal"><span class="textup">_</span><span class="textdown"> -</span></span>
-<span id="jkeyshiftd" class="normal" style="width: 82px;">Shift</span>
+<span id="jkeycomma" class="normal"><span class="textup">;</span><span class="textdown">,</span></span>
+<span id="jkeyperiod" class="normal"><span class="textup">:</span><span class="textdown">.</span></span>
+<span id="jkeyminus" class="normal"><span class="textup">_</span><span class="textdown">-</span></span>
+<span id="jkeyshiftd" class="normal" style="width: 107px; border-right-style: solid;">Shift</span>
 <br>
 <span id="jkeyctrll" class="normal" style="width: 80px;">Ctrl</span>
 <span id="jkeyfn" class="normal">Fn</span>
 <span id="jkeyalt" class="normal" style="width: 45px;">Alt</span>
 <span id="jkeyspace" class="normal" style="width: 240px;">Space</span>
-<span id="jkeyaltgr" class="normal" style="width: 50px;">Alt gr</span>
+<span id="jkeyaltgr" class="normal" style=" font-size: 12px !important; color:blue; width: 50px;">Alt gr</span>
 <span id="jkeyfn" class="normal">Fn</span>
 <span id="jempty" class="normal" style="width: 30px;">&nbsp;</span>
-<span id="jkeyctrlr" class="normal" style="width: 80px; border-right-style: solid;">Ctrl</span><br>
+<span id="jkeyctrlr" class="normal" style="width: 80px;">Ctrl</span><br>
 </span>
 </div>
