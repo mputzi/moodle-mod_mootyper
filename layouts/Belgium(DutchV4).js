ae95821a1527dad85badf57c0cc483d318dec6a9
alrachels <drachels@drachels.com>
2017-11-17 11:45:23 -0600

    New V4 Belgium layout.

---
+++ layouts/Belgium(DutchV4).js
@@ -0,0 +1,314 @@
+var THE_LAYOUT,
+    ended = false,
+    started = false,
+    doStart,
+    getPressedChar,
+    combinedChar,
+    combinedCharWait,
+    $,
+    currentChar,
+    show_keyboard,
+    currentPos,
+    fullText,
+    doKonec,
+    moveCursor,
+    napake,
+    keyupFirst,
+    event;
+
+/**
+ * Check for combined character.
+ * @param {char} chr.
+ * @returns {char}.
+ */
+function isCombined(chr) {
+    return (chr === '´' || chr === '`' || chr === '~');
+}
+
+THE_LAYOUT = 'Belgium(DutchV4)';
+
+/**
+ * Process keyup for combined character.
+ * @param {char} e.
+ * @returns {bolean}.
+ */
+function keyupCombined(e) {
+    if (ended) {
+        return false;
+    }
+    if (!started) {
+        doStart();
+    }
+    var keychar = getPressedChar(e);
+    if (keychar === '[not_yet_defined]') {
+        combinedChar = true;
+        return true;
+    }
+    if (combinedCharWait) {
+        combinedCharWait = false;
+        return true;
+    }
+    var currentText = $('#tb1').val();
+    var lastChar = currentText.substring(currentText.length - 1);
+    if (combinedChar && lastChar === currentChar) {
+        if (show_keyboard) {
+            var thisE = new keyboardElement(currentChar);
+            thisE.turnOff();
+        }
+        if (currentPos === fullText.length - 1) {   // END.
+            doKonec();
+            return true;
+        }
+        if (currentPos < fullText.length - 1) {
+            var nextChar = fullText[currentPos + 1];
+            if (show_keyboard){
+                var nextE = new keyboardElement(nextChar);
+                nextE.turnOn();
+            }
+            if (!isCombined(nextChar)) {            // If next char is not combined char.
+                $("#form1").off("keyup", "#tb1");
+                $("#form1").on("keypress", "#tb1", keyPressed);
+            }
+        }
+        combinedChar = false;
+        moveCursor(currentPos + 1);
+        currentChar = fullText[currentPos + 1];
+        currentPos++;
+        return true;
+    } else {
+        combinedChar = false;
+        napake++;
+        var tbval = $('#tb1').val();
+        $('#tb1').val(tbval.substring(0, currentPos));
+        return false;
+    }
+}
+
+/**
+ * Process keyupFirst.
+ * @param {char} event.
+ * @returns {bolean}.
+ */
+function keyupFirst(event) {
+    $("#form1").off("keyup", "#tb1", keyupFirst);
+    $("#form1").on("keyup", "#tb1", keyupCombined);
+    return false;
+}
+
+/**
+ * Check for character typed so flags can be set.
+ * @param {char} ltr.
+ */
+function keyboardElement(ltr) {
+    this.chr = ltr.toLowerCase();
+    this.alt = false;
+    this.accent = false;
+    if (isLetter(ltr)) {
+        this.shift = ltr.toUpperCase() === ltr;
+    } else {
+        // @codingStandardsIgnoreLine
+        if (ltr.match(/[³1234567890°_¨*%£<?./+]/i)) {
+            this.shift = true;
+        } else {
+            this.shift = false;
+        }
+    }
+    // @codingStandardsIgnoreLine
+    if (ltr.match(/[\\|@#€{}\[\]~´`ñ]/i)) {
+        this.shift = false;
+        this.alt = true;
+        this.accent = false;
+    }
+    // @codingStandardsIgnoreLine
+    if (ltr.match(/[ëïöü]/i)) {
+        this.shift = true;
+        this.alt = false;
+        this.accent = false;
+        this.caret = true;
+    }
+    if (ltr === 'ê') {
+        this.shift = false;
+        this.alt = false;
+        this.accent = false;
+        this.caret = true;
+    }
+    if (ltr === 'ó' || ltr === 'á') {
+        this.shift = false;
+        this.alt = true;
+        this.accent = true;
+    }
+    if (ltr === 'ñ') {
+        this.shift = false;
+        this.alt = true;
+        this.accent = false;
+        this.tilde = true;
+    }
+    this.turnOn = function() {
+        if (isLetter(this.chr)) {
+            document.getElementById(getKeyID(this.chr)).className = "next" + thenFinger(this.chr.toLowerCase());
+        } else if (this.chr === ' ') {
+            document.getElementById(getKeyID(this.chr)).className = "nextSpace";
+        } else {
+            document.getElementById(getKeyID(this.chr)).className = "next" + thenFinger(this.chr.toLowerCase());
+        }
+
+        if (this.chr === '\n' || this.chr === '\r\n' || this.chr === '\n\r' || this.chr === '\r') {
+            document.getElementById('jkeyenter').className = "next4";
+        }
+        if (this.shift) {
+            document.getElementById('jkeyshiftd').className = "next4";
+            document.getElementById('jkeyshiftl').className = "next4";
+        }
+        if (this.alt) {
+            document.getElementById('jkeyaltgr').className = "next2";
+        }
+        if (this.accent) {
+            document.getElementById('jkeyù').className = "next4";
+        }
+        if (this.caret) {
+            document.getElementById('jkeycaret').className = "next4";
+        }
+        if (this.tilde) {
+            document.getElementById('jkeyequal').className = "next4";
+        }
+    };
+    this.turnOff = function() {
+        if (isLetter(this.chr)) {
+            // @codingStandardsIgnoreLine
+            if (this.chr.match(/[qsdfjklm]/i)) {
+                document.getElementById(getKeyID(this.chr)).className = "finger" + thenFinger(this.chr.toLowerCase());
+            } else {
+                document.getElementById(getKeyID(this.chr)).className = "normal";
+            }
+        } else {
+            document.getElementById(getKeyID(this.chr)).className = "normal";
+        }
+
+        if (this.chr === '\n' || this.chr === '\r\n' || this.chr === '\n\r' || this.chr === '\r') {
+            document.getElementById('jkeyenter').classname = "normal";
+        }
+        if (this.shift) {
+            document.getElementById('jkeyshiftd').className = "normal";
+            document.getElementById('jkeyshiftl').className = "normal";
+        }
+        if (this.alt) {
+            document.getElementById('jkeyaltgr').className = "normal";
+        }
+        if (this.accent) {
+            document.getElementById('jkeyù').className = "normal";
+        }
+        if (this.caret) {
+            document.getElementById('jkeycaret').className = "normal";
+        }
+        if (this.tilde) {
+            document.getElementById('jkeyequal').className = "normal";
+        }
+    };
+}
+
+/**
+ * Set color flag based on current character.
+ * @param {char} tCrka.
+ * @returns {int}.
+ */
+function thenFinger(tCrka) {
+    if (tCrka === ' ') {
+        return 5; // Highlight the spacebar.
+        // @codingStandardsIgnoreLine
+    } else if (tCrka.match(/[²³&1|aáqw<>\\à0}pm)°^¨\[ù%´=+~\-_$*\]µ£`]/i)) {
+        return 4; // Highlight the correct key above in red.
+        // @codingStandardsIgnoreLine
+    } else if (tCrka.match(/[é2@zsxç9{oóöl:/]/i)) {
+        return 3; // Highlight the correct key above in green.
+        // @codingStandardsIgnoreLine
+    } else if (tCrka.match(/["3#eéë€êdc!8iíïk;.]/i)) {
+        return 2; // Highlight the correct key above in yellow.
+        // @codingStandardsIgnoreLine
+    } else if (tCrka.match(/[\'4rf(5tgbv§6yhnñè7uúüj,?]/i)) {
+        return 1; // Highlight the correct key above in blue.
+    } else {
+        return 6;
+    }
+}
+
+/**
+ * Get ID of key to highlight based on current character.
+ * @param {char} tCrka.
+ * @returns {varchar}.
+ */
+function getKeyID(tCrka) {
+    if (tCrka === ' ') {
+        return "jkeyspace";
+    } else if (tCrka === '\n') {
+        return "jkeyenter";
+    } else if (tCrka === ',' || tCrka === '?') {
+        return "jkeycomma";
+    } else if (tCrka === ';' || tCrka === '.') {
+        return "jkeysemicolon";
+    } else if (tCrka === ':' || tCrka === '/') {
+        return "jkeycolon";
+    } else if (tCrka === '-' || tCrka === '_') {
+        return "jkeyminus";
+    } else if (tCrka === '=' || tCrka === '+' || tCrka === '~') {
+        return "jkeyequal";
+    } else if (tCrka === '&' || tCrka === '|') {
+        return "jkey1";
+    } else if (tCrka === 'é' || tCrka === '@') {
+        return "jkey2";
+    } else if (tCrka === '"' || tCrka === '#') {
+        return "jkey3";
+    } else if (tCrka === '\'') {
+        return "jkey4";
+    } else if (tCrka === '(') {
+        return "jkey5";
+    } else if (tCrka === '§') {
+        return "jkey6";
+    } else if (tCrka === 'è') {
+        return "jkey7";
+    } else if (tCrka === '!') {
+        return "jkey8";
+    } else if (tCrka === 'ç' || tCrka === '{') {
+        return "jkey9";
+    } else if (tCrka === 'à' || tCrka === '}') {
+        return "jkey0";
+    } else if (tCrka === '^' || tCrka === '¨' || tCrka === '[') {
+        return "jkeycaret";
+    } else if (tCrka === '$' || tCrka === '*' || tCrka === ']') {
+        return "jkeydollar";
+    } else if (tCrka === '%' || tCrka === '´') {
+        return "jkeyù";
+    } else if (tCrka === '£' || tCrka === '`') {
+        return "jkeyµ";
+    } else if (tCrka === ')' || tCrka === '°') {
+        return "jkeyparenr";
+    } else if (tCrka === '<' || tCrka === '>') {
+        return "jkeyckck";
+    } else if (tCrka === '²' || tCrka === '³') {
+        return "jkeytildo";
+    } else if (tCrka === 'a' || tCrka === 'á') {
+        return "jkeya";
+    } else if (tCrka === '¨') {
+        return "jkeycaret";
+    } else if (tCrka === '€' || tCrka === 'é' || tCrka === 'ë' || tCrka === 'ê') {
+        return "jkeye";
+    } else if (tCrka === 'i' || tCrka === 'í' || tCrka === 'ï') {
+        return "jkeyi";
+    } else if (tCrka === 'ñ') {
+        return "jkeyn";
+    } else if (tCrka === 'o' || tCrka === 'ó' || tCrka === 'ö') {
+        return "jkeyo";
+    } else if (tCrka === 'u' || tCrka === 'ú' || tCrka === 'ü') {
+        return "jkeyu";
+    } else {
+        return "jkey" + tCrka;
+    }
+}
+
+/**
+ * Is the typed letter part of the current alphabet.
+ * @param {char} str.
+ * @returns {(int|Array)}.
+ */
+function isLetter(str) {
+    return str.length === 1 && str.match(/[0-9a-z¡ñçáéíóúüùµ]/i);
+}
---
+++ layouts/Belgium(DutchV4).php
@@ -0,0 +1,110 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file defines the Belgium(DutchV4) keyboard layout.
+ *
+ * @package    mod_mootyper
+ * @copyright  2016 onwards AL Rachels (drachels@drachels.com)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ require_once(dirname(dirname(dirname(dirname(__FILE__)))).'/config.php');
+ require_login($course, true, $cm);
+?>
+<div id="innerKeyboard" style="margin: 0px auto;display: inline-block;
+<?php
+echo (isset($displaynone) && ($displaynone == true)) ? 'display:none;' : '';
+?>
+"><br>
+<div id="keyboard" class="keyboardback">Belgium(DutchV4) Keyboard Layout<br>
+    <section>
+        <div class="row" style='float: left; margin-left:5px; font-size: 12px !important; line-height: 15px'>
+            <div id="jkeytildo" class="normal">³<br>²</div>
+            <div id="jkey1" class="normal" style='text-align:left;'>1<br>&<span style="color:blue">&nbsp; &nbsp; &nbsp;|</span></div>
+            <div id="jkey2" class="normal" style='text-align:left;'>2<br>é<span style="color:blue">&nbsp; &nbsp; &nbsp;@</span></div>
+            <div id="jkey3" class="normal" style='text-align:left;'>3<br>"<span style="color:blue">&nbsp; &nbsp; &nbsp;#</span></div>
+            <div id="jkey4" class="normal">4<br>'</div>
+            <div id="jkey5" class="normal">5<br>(</div>
+            <div id="jkey6" class="normal" style='text-align:left;'>6<br>§<span style="color:red">&nbsp; &nbsp; &nbsp;^</span></div>
+            <div id="jkey7" class="normal">7<br>è</div>
+            <div id="jkey8" class="normal">8<br>!</div>
+            <div id="jkey9" class="normal" style='text-align:left;'>9<br>ç<span style="color:blue">&nbsp; &nbsp; &nbsp;{</span></div>
+            <div id="jkey0" class="normal" style='text-align:left;'>0<br>à<span style="color:blue">&nbsp; &nbsp; &nbsp;}</span></div>
+            <div id="jkeyparenr" class="normal">°<br>)</div>
+            <div id="jkeyminus" class="normal">_<br>-</div>
+            <div id="jkeybackspace" class="normal"  style="width: 95px;">Backspace</div>
+        </div>
+        <div style="float: left;">
+            <div class="row" style='float: left; margin-left:5px;'>
+                <div id="jkeytab" class="normal" style="width: 60px;">Tab</div>
+                <div id="jkeya" class="normal">A</div>
+                <div id="jkeyz" class="normal">Z</div>
+                <div id="jkeye" class="normal">E<span style="font-size: 12px !important; color:blue">&nbsp;&nbsp;€</span></div>
+                <div id="jkeyr" class="normal">R</div>
+                <div id="jkeyt" class="normal">T</div>
+                <div id="jkeyy" class="normal">Y</div>
+                <div id="jkeyu" class="normal">U</div>
+                <div id="jkeyi" class="normal">I</div>
+                <div id="jkeyo" class="normal">O</div>
+                <div id="jkeyp" class="normal">P</div>
+                <div id="jkeycaret" class="normal" style="text-align:left; font-size: 12px !important;"><span style="color:red">¨<br>^<span style="color:blue">&nbsp; &nbsp; &nbsp;[</span></div>
+                <div id="jkeydollar" class="normal" style="text-align:left;font-size: 12px !important;">*<br>$<span style="color:blue">&nbsp; &nbsp; &nbsp;]</div>
+            </div>
+            <span id="jkeyenter" class="normal" style="width: 50px; margin-right:5px; float: right; height: 85px;">Enter</span>
+            <div class="row" style='float: left; margin-left:5px;'>
+                <div id="jkeycaps" class="normal" style="width: 80px; font-size: 12px !important;">Caps Lock</div>
+                <div id="jkeyq" class="finger4">Q</div>
+                <div id="jkeys" class="finger3">S</div>
+                <div id="jkeyd" class="finger2">D</div>
+                <div id="jkeyf" class="finger1">F</div>
+                <div id="jkeyg" class="normal">G</div>
+                <div id="jkeyh" class="normal">H</div>
+                <div id="jkeyj" class="finger1">J</div>
+                <div id="jkeyk" class="finger2">K</div>
+                <div id="jkeyl" class="finger3">L</div>
+                <div id="jkeym" class="finger4">M</div>
+                <div id="jkeyù" class="normal" style='text-align:left; font-size: 12px !important; line-height: 15px'>%<br>ù<span style="color:red">&nbsp; &nbsp; &nbsp;´</span></div>
+                <div id="jkeyµ" class="normal" style='text-align:left; font-size: 12px !important; line-height: 15px'>£<br>µ<span style="color:red">&nbsp; &nbsp; &nbsp;`</span></div>
+            </div>
+        </div>
+        <div class="row" style='float: left; margin-left:5px;'>
+            <div id="jkeyshiftl" class="normal" style="width: 70px;">Shift</div>
+            <div id="jkeyckck" class="normal" style='text-align:left; font-size: 12px !important; line-height: 15px'>&lt;<br>&gt;<span style="color:blue">&nbsp; &nbsp; &nbsp;\</span></div>
+            <div id="jkeyw" class="normal">W</div>
+            <div id="jkeyx" class="normal">X</div>
+            <div id="jkeyc" class="normal">C</div>
+            <div id="jkeyv" class="normal">V</div>
+            <div id="jkeyb" class="normal">B</div>
+            <div id="jkeyn" class="normal">N</div>
+            <div id="jkeycomma" class="normal" style='font-size: 12px !important; line-height: 15px'>?<br>,</div>
+            <div id="jkeysemicolon" class="normal" style='font-size: 12px !important; line-height: 15px'>.<br>;</div>
+            <div id="jkeycolon" class="normal" style='font-size: 12px !important; line-height: 15px'>/<br>:</div>
+            <div id="jkeyequal" class="normal" style='text-align:left; font-size: 12px !important; line-height: 15px'>+<br>=<span style="color:red">&nbsp; &nbsp; &nbsp;~</span></div>
+            <div id="jkeyshiftd" class="normal" style="width: 105px; border-right-style: solid;">Shift</div>
+        </div>
+        <div class="row" style='float: left; margin-left:5px;'>
+            <div id="jkeyctrll" class="normal" style="width: 50px;">Ctrl</div>
+            <div id="jkeyfn" class="normal" style="width: 50px;">Win</div>
+            <div id="jkeyalt" class="normal" style="width: 50px;">Alt</div>
+            <div id="jkeyspace" class="normal" style="width: 265px;">Space</div>
+            <div id="jkeyaltgr" class="normal" style=" font-size: 12px !important; color:blue; width: 50px;">Alt gr</div>
+            <div id="jkeyfn" class="normal" style="width: 50px;">Win</div>
+            <div id="jempty" class="normal" style="width: 50px;">Menu</div>
+            <div id="jkeyctrlr" class="normal" style="width: 50px;">Ctrl</div>
+        </div>
+    </section>
+</div>
+</div>
