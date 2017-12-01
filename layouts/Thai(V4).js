0357da1dc163c3b5271abb9c1c9af341b41f596a
alrachels <drachels@drachels.com>
2017-12-01 16:13:48 -0600

    New Thai layout. Work in progress.

---
+++ layouts/Thai(V4).js
@@ -0,0 +1,202 @@
+var THE_LAYOUT,
+    ended = false,
+    started = false,
+    doStart,
+    getPressedChar,
+    combinedChar,
+    combinedCharWait,
+    $,
+    currentChar,
+    show_keyboard,
+    currentPos,
+    fullText,
+    doKonec,
+    moveCursor,
+    napake,
+    keyupFirst,
+    event;
+
+/**
+ * Check for combined character.
+ * @param {char} chr.
+ * @returns {char}.
+ */
+function isCombined(chr) {
+    return false;
+}
+
+/**
+ * Process keyup for combined character.
+ * @param {char} e.
+ * @returns {bolean}.
+ */
+function keyupCombined(e) {
+    return false;
+}
+
+/**
+ * Process keyupFirst.
+ * @param {char} event.
+ * @returns {bolean}.
+ */
+function keyupFirst(event) {
+    return false;
+}
+
+THE_LAYOUT = 'Thai(V4)';
+
+/**
+ * Check for character typed so flags can be set.
+ * @param {char} ltr.
+ */
+function keyboardElement(ltr) {
+    this.chr = ltr.toLowerCase();
+    this.alt = false;
+    if (isLetter(ltr)) {
+        this.shift = ltr.toUpperCase() === ltr;
+    } else {
+        // @codingStandardsIgnoreLine
+        if (ltr.match(/[%()+๐"ฎฑธํ๊ณฯญฐ,ฅ]/i)) {
+            this.shift = true;
+        } else {
+            this.shift = false;
+        }
+    }
+    this.turnOn = function() {
+        if (isLetter(this.chr)) {
+            document.getElementById(getKeyID(this.chr)).className = "next" + thenFinger(this.chr.toLowerCase());
+        } else if (this.chr === ' ') {
+            document.getElementById(getKeyID(this.chr)).className = "nextSpace";
+        } else {
+            document.getElementById(getKeyID(this.chr)).className = "next" + thenFinger(this.chr.toLowerCase());
+        }
+        if (this.chr === '\n' || this.chr === '\r\n' || this.chr === '\n\r' || this.chr === '\r') {
+            document.getElementById('jkeyenter').className = "next4";
+        }
+        if (this.shift) {
+            document.getElementById('jkeyshiftd').className = "next4";
+            document.getElementById('jkeyshiftl').className = "next4";
+        }
+        if (this.alt) {
+            document.getElementById('jkeyaltgr').className = "nextSpace";
+        }
+    };
+    this.turnOff = function() {
+        if (isLetter(this.chr)) {
+        // @codingStandardsIgnoreLine
+//            if (this.chr.match(/[asdfjkl;]/i)) {
+            if (this.chr.match(/[ฟหกด่าสว]/i)) {
+                document.getElementById(getKeyID(this.chr)).className = "finger" + thenFinger(this.chr.toLowerCase());
+            } else {
+                document.getElementById(getKeyID(this.chr)).className = "normal";
+            }
+        } else {
+            document.getElementById(getKeyID(this.chr)).className = "normal";
+        }
+        if (this.chr === '\n' || this.chr === '\r\n' || this.chr === '\n\r' || this.chr === '\r') {
+            document.getElementById('jkeyenter').classname = "normal";
+        }
+        if (this.shift) {
+            document.getElementById('ุ').className = "normal";
+            document.getElementById('jkeyshiftl').className = "normal";
+        }
+        if (this.alt) {
+            document.getElementById('jkeyaltgr').className = "normal";
+        }
+    };
+}
+
+/**
+ * Set color flag based on current character.
+ * @param {char} tCrka.
+ * @returns {number}.
+ */
+function thenFinger(tCrka) {
+    if (tCrka === ' ') {
+        return 5; // Highlight the spacebar.
+    // @codingStandardsIgnoreLine
+//    } else if (tCrka.match(/[`~1!qaz0)p;:/?\-_[{'"=+\]}\\|]/i)) {
+    } else if (tCrka.match(/[_%ๅ+ๆ๐ฟฤผ(จ๗ยญวซฝฦข๘บฐง.ช๙ล,ฃฅ]/i)) {
+        return 4; // Highlight the correct key above in red.
+    // @codingStandardsIgnoreLine
+//    } else if (tCrka.match(/[2@wsx9(ol.>]/i)) {
+    } else if (tCrka.match(/[/๑ไ"หฆป)ต๖นฯสศใฬ]/i)) {
+        return 3; // Highlight the correct key above in green.
+    // @codingStandardsIgnoreLine
+//    } else if (tCrka.match(/[3#edc8*ik,<]/i)) {
+    } else if (tCrka.match(/[-๒ำฎกฏแฉค๕รณาษมฒ]/i)) {
+        return 2; // Highlight the correct key above in yellow.
+    // @codingStandardsIgnoreLine
+//    } else if (tCrka.match(/[4$rfv5%tgb6^yhn7&ujm]/i)) {
+    } else if (tCrka.match(/[ภ๓พฑดโอฮถ๔ะธเฌิฺุูัํ้็ื์ึ฿ี๊่๋ท?]/i)) {
+        return 1; // Highlight the correct key above in blue.
+    } else {
+        return 6; // Do not change any highlight.
+    }
+}
+
+/**
+ * Get ID of key to highlight based on current character.
+ * @param {char} tCrka.
+ * @returns {char}.
+ */
+function getKeyID(tCrka) {
+    if (tCrka === ' ') {
+        return "jkeyspace";
+    } else if (tCrka === '\n') {
+        return "jkeyenter";
+    } else if (tCrka === '_' || tCrka === '%') {
+        return "jkeybackquote";
+    } else if (tCrka === 'ๅ' || tCrka === '+') {
+        return "jkey1";
+    } else if (tCrka === '/' || tCrka === '๑') {
+        return "jkey2";
+    } else if (tCrka === '-' || tCrka === '๒') {
+        return "jkey3";
+    } else if (tCrka === 'ภ' || tCrka === '๓') {
+        return "jkey4";
+    } else if (tCrka === 'ถ' || tCrka === '๔') {
+        return "jkey5";
+    } else if (tCrka === 'ุ' || tCrka === 'ู') {
+        return "jkey6";
+    } else if (tCrka === 'ึ' || tCrka === '฿') {
+        return "jkey7";
+    } else if (tCrka === 'ค' || tCrka === '๕') {
+        return "jkey8";
+    } else if (tCrka === 'ต' || tCrka === '๖') {
+        return "jkey9";
+    } else if (tCrka === 'จ' || tCrka === '๗') {
+        return "jkey0";
+    } else if (tCrka === 'ข' || tCrka === '๘') {
+        return "jkeyminus";
+    } else if (tCrka === 'ช' || tCrka === '๙') {
+        return "jkeyequals";
+    } else if (tCrka === 'บ' || tCrka === 'ฐ') {
+        return "jkeybracketl";
+    } else if (tCrka === 'ล' || tCrka === ',') {
+        return "jkeybracketr";
+    } else if (tCrka === "ฃ" || tCrka === 'ฅ') {
+        return "jkeybackslash";
+    } else if (tCrka === 'ว' || tCrka === 'ซ') {
+        return "jkeysemicolon";
+    } else if (tCrka === "ง" || tCrka === '.') {
+        return "jkeycrtica";
+    } else if (tCrka === 'ม' || tCrka === 'ฒ') {
+        return "jkeycomma";
+    } else if (tCrka === 'ใ' || tCrka === 'ฬ') {
+        return "jkeyperiod";
+    } else if (tCrka === 'ฝ' || tCrka === 'ฦ') {
+        return "jkeyslash";
+    } else {
+        return "jkey" + tCrka;
+    }
+}
+
+/**
+ * Is the typed letter part of the current alphabet.
+ * @param {char} str.
+ * @returns {(int|Array)}.
+ */
+function isLetter(str) {
+    return str.length === 1 && str.match(/[ก-ฮ]/i);
+}
