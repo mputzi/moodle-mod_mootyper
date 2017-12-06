24b9d2b6fc5fb0dd476135aaf3398330370930ca
alrachels <drachels@drachels.com>
2017-12-06 09:43:41 -0600

    Fixed reversed characters.

--- layouts/Portuguese(PortugalV4).php
+++ layouts/Portuguese(PortugalV4).php
@@ -29,7 +29,7 @@
 echo (isset($displaynone) && ($displaynone == true)) ? 'display:none;' : '';
 ?>
 ">
-<div id="keyboard" class="keyboardback">Portuguese(PortugalV4) Keyboard Layout<br>
+<div id="keyboard" class="keyboardback">Teclado Português (Portugal - V4)<br>
     <section>
         <div class="row" style='float: left; margin-left:5px; font-size: 15px !important; line-height: 15px'>
             <div id="jkeybackslash" class="normal" style='text-align:left;'>|<br>\</div>
@@ -38,7 +38,7 @@ echo (isset($displaynone) && ($displaynone == true)) ? 'display:none;' : '';
             <div id="jkey3" class="normal" style='text-align:left;'>#<br>3<span style="color:blue">&nbsp; &nbsp;£</span></div>
             <div id="jkey4" class="normal" style='text-align:left;'>$<br>4<span style="color:blue">&nbsp; &nbsp;§</span></div>
             <div id="jkey5" class="normal" style='text-align:left;'>%<br>5<span style="color:blue">&nbsp; &nbsp;€</span></div>
-            <div id="jkey6" class="normal" style='text-align:left;'>&<br>6</div>
+            <div id="jkey6" class="normal" style='text-align:left;'>& <br>6</div>
             <div id="jkey7" class="normal" style='text-align:left;'>/<br>7<span style="color:blue">&nbsp;&nbsp;&nbsp;{</span></div>
             <div id="jkey8" class="normal" style='text-align:left;'>(<br>8<span style="color:blue">&nbsp;&nbsp;&nbsp;[</span></div>
             <div id="jkey9" class="normal" style='text-align:left;'>)<br>9<span style="color:blue">&nbsp;&nbsp;&nbsp;]</span></div>
