d6d4c5e7c6a98b0720a68d0fd2cbe730ef596b94
drachels <drachels@drachels.com>
2019-03-10 12:34:11 -0500

    Code style cleanup.

--- lsnimport.php
+++ lsnimport.php
@@ -29,9 +29,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
  */
 
-require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-require_once(dirname(__FILE__).'/lib.php');
-require_once(dirname(__FILE__).'/locallib.php');
+require(__DIR__ . '/../../config.php');
+require_once(__DIR__ . '/lib.php');
+require_once(__DIR__ . '/locallib.php');
 
 /**
  * Define the lesson import function.
