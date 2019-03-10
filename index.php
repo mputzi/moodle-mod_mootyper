061d441a76dc6fa151bfd1fe1335e8f2d57f135d
drachels <drachels@drachels.com>
2019-03-10 12:33:09 -0500

    Code style cleanup.

--- index.php
+++ index.php
@@ -23,8 +23,9 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later.
  */
 
-require_once(dirname(dirname(dirname(__FILE__))).'/config.php');
-require_once(dirname(__FILE__).'/lib.php');
+// Changed to this newer format 03/10/2019.
+require(__DIR__ . '/../../config.php');
+require_once(__DIR__ . '/lib.php');
 
 $id = required_param('id', PARAM_INT);   // Course.
 
