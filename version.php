e3b84d9bd15cf48d591a422d70dbc770b25739a2
drachels <drachels@drachels.com>
2019-03-15 16:17:05 -0500

    Version bump.

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2019030300;     // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2019030300.03;     // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;     // Requires Moodle 2.7.
 $plugin->cron      = 60;             // Period for cron to check this module (secs).
 $plugin->component = 'mod_mootyper';
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '3.6.0 (Build: 2019030300)';
+$plugin->release   = '3.6.0+ (Build: 2019030300.03)';
