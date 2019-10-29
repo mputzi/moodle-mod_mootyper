ec01ab20a58e57a67e6a67d0deb935f1662a5aeb
drachels <drachels@drachels.com>
2019-10-29 13:36:08 -0500

    Rewrote event code.

--- attrem.php
+++ attrem.php
@@ -57,20 +57,14 @@ if (isset($gradeid)) {
 
 // Return to the View my grades or View all grades page.
 if ($mtmode == 2) {
-    // Trigger grade deleted event for mode 2.
-    $params = array('objectid' => $mootyper->id,
-       'context' => $context,
-       'mode' => $mootyper->isexam
-    );
+    // Trigger grade deleted event for mode 2 only if on, View own grades, page.
+    $params = array('objectid' => $mootyper->id, 'context' => $context, 'mode' => $mootyper->isexam);
     $event = owngrades_deleted::create($params);
     $event->trigger();
     $webdir = $CFG->wwwroot . '/mod/mootyper/owngrades.php?id='.$cid.'&n='.$mid;
 } else {
-    // Trigger grade deleted event for mode 0 or 1.
-        $params = array('objectid' => $mootyper->id,
-       'context' => $context,
-       'mode' => $mootyper->isexam
-    );
+    // Trigger grade deleted event for mode 0, 1, or 2 if on, View all grades, page.
+    $params = array('objectid' => $mootyper->id, 'context' => $context, 'mode' => $mootyper->isexam);
     $event = grade_deleted::create($params);
     $event->trigger();
     $webdir = $CFG->wwwroot . '/mod/mootyper/gview.php?id='.$cid.'&n='.$mid;
