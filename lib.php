6842d244cf37418d7e6215fce844763452c86901
drachels <drachels@drachels.com>
2020-04-25 10:45:46 -0500

    Turn off unused features.

--- lib.php
+++ lib.php
@@ -57,13 +57,13 @@ function mootyper_supports($feature) {
         case FEATURE_COMPLETION_TRACKS_VIEWS:
             return true;
         case FEATURE_COMPLETION_HAS_RULES:
-            return true;
+            return false;
         case FEATURE_GRADE_HAS_GRADE:
-            return true;
+            return false;
         case FEATURE_GRADE_OUTCOMES:
-            return true;
+            return false;
         case FEATURE_RATE:
-            return true;
+            return false;
         case FEATURE_BACKUP_MOODLE2:
             return true;
         case FEATURE_SHOW_DESCRIPTION:
