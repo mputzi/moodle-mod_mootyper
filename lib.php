5472c816fa32a7e25c3d253aa15fed1cbf5aad68
AL Rachels <drachels@drachels.com>
2022-03-06 10:59:35 -0600

    Update lib.php

--- lib.php
+++ lib.php
@@ -41,33 +41,44 @@ define('MOOTYPER_EVENT_TYPE_CLOSE', 'close');
 /**
  * Returns the information on whether the module supports a feature.
  *
- * @see plugin_supports() in lib/moodlelib.php
- * @param string $feature FEATURE_xx constant for requested feature
- * @return mixed true if the feature is supported, null if unknown
+ * @uses FEATURE_BACKUP_MOODLE2
+ * @uses FEATURE_COMPLETION_TRACKS_VIEWS
+ * @uses FEATURE_COMPLETION_HAS_RULES
+ * @uses FEATURE_GRADE_HAS_GRADE
+ * @uses FEATURE_GRADE_OUTCOMES
+ * @uses FEATURE_GROUPS
+ * @uses FEATURE_GROUPINGS
+ * @uses FEATURE_GROUPMEMBERSONLY
+ * @uses FEATURE_MOD_INTRO
+ * @uses FEATURE_RATE
+ * @uses FEATURE_SHOW_DESCRIPTION
+ * @param string $feature
+ * @return mixed True if yes (some features may use other values)
  */
+
 function mootyper_supports($feature) {
     global $CFG;
 
     switch ($feature) {
-        case FEATURE_GROUPS;
-            return true;
-        case FEATURE_GROUPINGS:
-            return false;
-        case FEATURE_GROUPMEMBERSONLY:
-            return false;
-        case FEATURE_MOD_INTRO:
+        case FEATURE_BACKUP_MOODLE2:
             return true;
         case FEATURE_COMPLETION_TRACKS_VIEWS:
             return true;
         case FEATURE_COMPLETION_HAS_RULES:
-            return false;
+            return true;
         case FEATURE_GRADE_HAS_GRADE:
             return true;
         case FEATURE_GRADE_OUTCOMES:
             return false;
-        case FEATURE_RATE:
+        case FEATURE_GROUPS;
             return true;
-        case FEATURE_BACKUP_MOODLE2:
+        case FEATURE_GROUPINGS:
+            return true;
+        case FEATURE_GROUPMEMBERSONLY:
+            return true;
+        case FEATURE_MOD_INTRO:
+            return true;
+        case FEATURE_RATE:
             return true;
         case FEATURE_SHOW_DESCRIPTION:
             return true;
