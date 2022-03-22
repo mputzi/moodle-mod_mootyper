abd9adaab8eac8b999f2934eb0389969b60a0f5a
AL Rachels <drachels@drachels.com>
2022-03-22 14:08:21 -0500

    Added switch branch ck

--- lib.php
+++ lib.php
@@ -58,33 +58,64 @@ define('MOOTYPER_EVENT_TYPE_CLOSE', 'close');
 
 function mootyper_supports($feature) {
     global $CFG;
+    if ($CFG->branch > 311) {
+        switch ($feature) {
+            case FEATURE_MOD_PURPOSE:
+                return MOD_PURPOSE_ASSESSMENT;
+            case FEATURE_BACKUP_MOODLE2:
+                return true;
+            case FEATURE_COMPLETION_TRACKS_VIEWS:
+                return true;
+            case FEATURE_COMPLETION_HAS_RULES:
+                return true;
+            case FEATURE_GRADE_HAS_GRADE:
+                return true;
+            case FEATURE_GRADE_OUTCOMES:
+                return false;
+            case FEATURE_GROUPS;
+                return true;
+            case FEATURE_GROUPINGS:
+                return true;
+            case FEATURE_GROUPMEMBERSONLY:
+                return true;
+            case FEATURE_MOD_INTRO:
+                return true;
+            case FEATURE_RATE:
+                return true;
+            case FEATURE_SHOW_DESCRIPTION:
+                return true;
 
-    switch ($feature) {
-        case FEATURE_BACKUP_MOODLE2:
-            return true;
-        case FEATURE_COMPLETION_TRACKS_VIEWS:
-            return true;
-        case FEATURE_COMPLETION_HAS_RULES:
-            return true;
-        case FEATURE_GRADE_HAS_GRADE:
-            return true;
-        case FEATURE_GRADE_OUTCOMES:
-            return false;
-        case FEATURE_GROUPS;
-            return true;
-        case FEATURE_GROUPINGS:
-            return true;
-        case FEATURE_GROUPMEMBERSONLY:
-            return true;
-        case FEATURE_MOD_INTRO:
-            return true;
-        case FEATURE_RATE:
-            return true;
-        case FEATURE_SHOW_DESCRIPTION:
-            return true;
+            default:
+                return null;
+        }
+    } else {
+        switch ($feature) {
+            case FEATURE_BACKUP_MOODLE2:
+                return true;
+            case FEATURE_COMPLETION_TRACKS_VIEWS:
+                return true;
+            case FEATURE_COMPLETION_HAS_RULES:
+                return true;
+            case FEATURE_GRADE_HAS_GRADE:
+                return true;
+            case FEATURE_GRADE_OUTCOMES:
+                return false;
+            case FEATURE_GROUPS;
+                return true;
+            case FEATURE_GROUPINGS:
+                return true;
+            case FEATURE_GROUPMEMBERSONLY:
+                return true;
+            case FEATURE_MOD_INTRO:
+                return true;
+            case FEATURE_RATE:
+                return true;
+            case FEATURE_SHOW_DESCRIPTION:
+                return true;
 
-        default:
-            return null;
+            default:
+                return null;
+        }
     }
 }
 
