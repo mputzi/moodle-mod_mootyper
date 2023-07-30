1df75d7de25720c3812699d549a07933de2f0b5e
AL Rachels <drachels@drachels.com>
2023-07-30 11:30:37 -0500

    Update version.php

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023041001; // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2023073000; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2018120300; // Requires Moodle 3.6.
 $plugin->cron = 60; // Period for cron to check this module (secs).
 $plugin->component = 'mod_mootyper';
-$plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.2.3+ (Build: 2023041001)';
+$plugin->maturity = MATURITY_BETA;
+$plugin->release = '4.2.4 (Build: 2023073000)';
